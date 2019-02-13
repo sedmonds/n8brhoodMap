2e6385401d3670dd2cb60ac6136d63f434751027
sedmonds <info@scottedmonds.com>
2019-02-13 09:38:50 -0500

    Add list box to page content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react"
 import Map from "./Map";
+import List from "./List";
 
 class Content extends Component {
   render() {
     return (
-      <Map />
-    )
+      <div className="content">
+        <List />
+        <Map />
+      </div>
+    );
   }
 }
 
---
+++ src/components/List.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class List extends React.Component {
+  render() {
+    // locations go here
+    return (<div><h2>list items</h2></div>);
+  }
+}
+
+export default List
