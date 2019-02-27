c817a7cd97c06e344770110e0a4ebdf273cc4994
sedmonds <info@scottedmonds.com>
2019-02-27 11:24:41 -0600

    Adds PropTypes for properties passed to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  };
+
   render() {
+    const {
+      title = 'Neighborhood Map: Puna District Grocers'
+    } = this.props;
+
     return (
       <header id="header">
-        <h1 id="title">Puna District</h1>
+        <h1 id="title">{title}</h1>
       </header>
     );
   }
