128c1f16cda8f8737320f78a4b686eae530675ef
sedmonds <info@scottedmonds.com>
2019-02-27 04:06:20 -0600

    Address venue name and address

--- src/components/List.js
+++ src/components/List.js
@@ -5,15 +5,18 @@ class List extends React.Component {
   render() {
     // list of venues
     let venues = this.props.venues;
+
     return (
       <div id="list">
         <h2>Venues</h2>
         <input type="text" />
         <ol>
-          {venues.map(ven => (
+          {venues.map(ven => (       
             <li key={ven.venue.id}>
               <div>
-                <h2>{ven.venue.name}</h2>
+
+                <p>Name: <a href="#">{ven.venue.name}</a></p>
+                <p>Address: {ven.venue.location.address}</p>
               </div>
             </li>
           ))}
