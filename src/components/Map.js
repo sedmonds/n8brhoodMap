dbe7148009cd9bed8b76a84942a705b1d79b1ceb
sedmonds <info@scottedmonds.com>
2019-02-27 06:23:21 -0600

    Adds content to infowindow (dummy Fn:  constructInfo) Yay!

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,11 @@ class Map extends React.Component {
   markers = [];
   addMarkers = venues => {
     if (window.google) {
-      for (let i = 0; i < venues.length; i++) {
+
+      // https://developers.google.com/maps/documentation/javascript/infowindows
+      let infowindow = new window.google.maps.InfoWindow();
+
+      for (let i = 0; i <  venues.length; i++) {
         let marker = new window.google.maps.Marker({
           position:{
           lat: venues[i].venue.location.lat,
@@ -12,11 +16,28 @@ class Map extends React.Component {
           },
           map: window.googleMapObject
         });
+
+        // infowindow opens on map object and add marker
+        marker.addListener('click', function() {
+          infowindow.setContent(`<div>
+          <p>Name: <a href="#">${venues[i].venue.name}</a></p>
+          <p>Address: ${venues[i].venue.location.address}</p>
+        </div>`);
+
+          infowindow.open(window.googleMapObject, marker);
+        });
+    
         this.markers.push(marker);
       }
     }
   };
 
+  constructInfo(venue) {
+     
+    // for future use instead of infowindow.setContent()
+  
+  }
+  
   render() {
     this.addMarkers(this.props.venues);
     return <div id="map" />;
