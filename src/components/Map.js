b639d47721a95ada8cce0255387530572f13ad99
sedmonds <info@scottedmonds.com>
2019-02-27 04:36:31 -0600

    Re-fixes marker issue. See all markers on page. Finally

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 
 class Map extends React.Component {
-  // markers = [];
-  // addMarkers = venues => {
-  //   console.log(window.google);
+  markers = [];
+  addMarkers = venues => {
+    if (window.google) {
+      for (let i = 0; i < venues.length; i++) {
+        let marker = new window.google.maps.Marker({
+          position:{
+          lat: venues[i].venue.location.lat,
+          lng: venues[i].venue.location.lng 
+          },
+          map: window.googleMapObject
+        });
+        this.markers.push(marker);
+      }
+    }
+  };
 
-  //   if (window.google) {
-  //     for (let i = 0; i < venues.length; i++) {
-  //       let marker = new window.google.maps.Marker({ position: { lat: venues[i].venue.location.lat, lng: venues[i].venue.location.lng
-  //         },
-  //         map: window.googleMapObject
-  //       });
-  //       this.markers.push(marker);
-  //     }
-  //   }
-  // };
   render() {
-    // this.addMarkers(this.props.locations);
+    this.addMarkers(this.props.venues);
     return <div id="map" />;
   }
 }
